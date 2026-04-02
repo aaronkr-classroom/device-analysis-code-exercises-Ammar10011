fa8e44dbc6fb484d3185abd619ae9a043252ea7c
Ammar10011 <ammarhossain348@gmail.com>
2026-04-02 13:07:09 +0900

    Add files via upload

---
+++ code 1.py
@@ -0,0 +1,21 @@
+# data_types.py
+a = 10      # int
+b = 3.14    # float
+c = "Hello" # str
+d = True    # bool
+
+print(f"{a} is a {type(a)}")
+print(f"{b} is a {type(b)}")
+print(f"{a} is a {type(c)}")
+print(f"{a} is a {type(a)}")
+
+#str
+e= 'Ammar'
+f= 'Hossain'
+name = e + " " + f
+g = name +" said,\"It's a beautiful day! "
+h = ' "How \'ya doin\' today?"\n\t"Good!"'
+print(g, "\n", h)
+
+# Bool - only False or 0 or "" is false
+i= Tr
